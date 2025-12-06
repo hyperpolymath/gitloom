0efa06781e426ecdfb3e4f222e0044a3752afda2
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2025-12-06 18:49:04 +0000

    Add kanren_facts and loom-init

---
+++ logic/kanren_facts.pl
@@ -0,0 +1,4 @@
+fixed_head(gitloom).
+created_orphan_branch(gitloom).
+repo_configured(gitloom).
+pipeline_present(gitloom).
---
+++ loom-init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export LOOM_ROLE="reweaver"
+export LOOM_CARE_MODE="true"
+export LOOM_SANDBOX="kinoite-reweaver"
+export LOOM_BADGES="head_held_high loom_repaired loom_perfected"
+echo "🧶 Loom initialized for role: $LOOM_ROLE"
